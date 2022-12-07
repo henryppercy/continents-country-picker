67c0b678eb4c072041c37b75313e3168bb239508
Henry <henryppercy@gmail.com>
2022-12-07 14:28:53 +0000

    Created ContinentsList component

---
+++ src/Components/ContinentsList/index.tsx
@@ -0,0 +1,17 @@
+const ContinentsList = () => {
+
+    return (
+        <ul className="flex flex-col gap-6 font-sans basis-64">
+            <li>Africa</li>
+            <li>Antarctica</li>
+            <li>Asia</li>
+            <li>Europe</li>
+            <li>North America</li>
+            <li>Oceania</li>
+            <li>South America</li>
+        </ul>      
+    );
+  }
+  
+  export default ContinentsList;
+  
