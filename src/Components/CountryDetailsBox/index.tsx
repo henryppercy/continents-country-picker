2824c36a0a47d06cd1bbf9b8ae96536772454432
Henry <henryppercy@gmail.com>
2022-12-07 14:29:20 +0000

    Created CountryDetailsBox component

---
+++ src/Components/CountryDetailsBox/index.tsx
@@ -0,0 +1,52 @@
+const CountryDetailsBox = () => {
+    return (
+        <div className="border-gray-300 border-4 w-60 h-100 grow rounded-lg">
+            <ul className="grid grid-rows-6 grid-cols-7 gap-2">
+                <li>Angola</li>
+                <li>Angola</li>
+                <li>Angola</li>
+                <li>Angola</li>
+                <li>Angola</li>
+                <li>Angola</li>
+                <li>Angola</li>
+                <li>Angola</li>
+                <li>Angola</li>
+                <li>Angola</li>
+                <li>Angola</li>
+                <li>Angola</li>
+                <li>Angola</li>
+                <li>Angola</li>
+                <li>Angola</li>
+                <li>Angola</li>
+                <li>Angola</li>
+                <li>Angola</li>
+                <li>Angola</li>
+                <li>Angola</li>
+                <li>Angola</li>
+                <li>Angola</li>
+                <li>Angola</li>
+                <li>Angola</li>
+                <li>Angola</li>
+                <li>Angola</li>
+                <li>Angola</li>
+                <li>Angola</li>
+                <li>Angola</li>
+                <li>Angola</li>
+                <li>Angola</li>
+                <li>Angola</li>
+                <li>Angola</li>
+                <li>Angola</li>
+                <li>Angola</li>
+                <li>Angola</li>
+                <li>Angola</li>
+                <li>Angola</li>
+                <li>Angola</li>
+                <li>Angola</li>
+                <li>Angola</li>
+            </ul>
+        </div>
+    );
+  }
+  
+  export default CountryDetailsBox;
+  
